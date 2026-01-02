739b2bde872698ef88f91114596526c9ca62c2b7
Martin <mart256@gmail.com>
2026-01-02 18:44:00 +0100

    Using libgcc without explicit path

--- printNum/compile.sh
+++ printNum/compile.sh
@@ -1,16 +1,13 @@
 AS='riscv32-unknown-elf-as -march=rv32i -mabi=ilp32'
 GCC='riscv32-unknown-elf-gcc -march=rv32i -mabi=ilp32'
 LD=riscv32-unknown-elf-ld
-ELF2HEX=riscv32-unknown-elf-elf2hex
-
-# depends on your toolchain installation
-PATH_TO_LIBGCC=~/informatica/risc-v/install/rv32i/lib/gcc/riscv32-unknown-elf/11.1.0
+HEXDUMP=hexdump
 
 $AS printshort.S -o printshort.o &&
 $AS start.S -o start.o &&
 $AS delay.S -o delay.o &&
 $GCC -r main.c -o main.o &&
-$LD main.o printshort.o digitalwrite.o delay.o -o calculator.elf -T printNum.ld -m elf32lriscv -nostdlib --no-relax $PATH_TO_LIBGCC/libgcc.a &&
-$ELF2HEX --bit-width 32 --input calculator.elf --output calculator.hex
+$GCC main.o printshort.o digitalwrite.o delay.o -o calculator.elf -nostdlib -lgcc -T printNum.ld &&
+$HEXDUMP -v -e '"%08x\n"' calculator.elf > calculator.hex
 
 rm start.o main.o printshort.o calculator.elf delay.o
